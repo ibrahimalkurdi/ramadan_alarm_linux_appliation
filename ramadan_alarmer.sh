7874a3ce8472d70d11a8ac475d65d93ba1d33cb9
ibrahimalkurdi <37270645+ibrahimalkurdi@users.noreply.github.com>
2019-05-07 15:32:25 +0200

    Update ramadan_alarmer.sh

--- ramadan_alarmer.sh
+++ ramadan_alarmer.sh
@@ -5,17 +5,19 @@ SUNSET_FILE='/home/bob/sunset_ramadan_2019.txt'
 CURR_MONTH_DAY=$(date | awk '{print $2,$3}')
 LOG_FILE='/tmp/ramadan_2019.txt'
 ATHAN_FILE='/home/bob/Music/athan2.mp4'
+DAWN_VOLUME="40"
+SUNSET_VOLUME="100"
 
 if [[ $1 == "dawn" ]]; then
   date  >> ${LOG_FILE}
   ALARM_DATE="$(grep "${CURR_MONTH_DAY}" ${DAWN_FILE})"
   ALARM_TIME="$(echo ${ALARM_DATE} | awk '{print $3}')"
-  VOULME="40"
+  VOULME="${DAWN_VOLUME}"
 elif [[ $1 == "sunset" ]]; then
   date  >> ${LOG_FILE}
   ALARM_DATE="$(grep "${CURR_MONTH_DAY}" ${SUNSET_FILE})"
   ALARM_TIME="$(echo ${ALARM_DATE} | awk '{print $3}')"
-  VOULME="100"
+  VOULME="${SUNSET_VOLUME}"
 else
   date  >> ${LOG_FILE}
   echo "the Ramadan script is failed!" >> ${LOG_FILE}
