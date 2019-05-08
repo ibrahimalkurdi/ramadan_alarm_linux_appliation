5bc2eb8e5e6e3951a40497e08b44871d66c73918
ibrahimalkurdi <37270645+ibrahimalkurdi@users.noreply.github.com>
2019-05-08 03:29:50 +0200

    Update ramadan_alarmer.sh

--- ramadan_alarmer.sh
+++ ramadan_alarmer.sh
@@ -5,19 +5,19 @@ SUNSET_FILE='./sunset_ramadan_2019.txt'
 CURR_MONTH_DAY=$(date | awk '{print $2,$3}')
 LOG_FILE='/tmp/ramadan_2019.txt'
 ATHAN_FILE='/home/bob/Music/athan2.mp4'
-DAWN_VOLUME="40"
-SUNSET_VOLUME="100"
+DAWN_VOLUME=40
+SUNSET_VOLUME=100
 
 if [[ $1 == "dawn" ]]; then
   date  >> ${LOG_FILE}
   ALARM_DATE="$(grep "${CURR_MONTH_DAY}" ${DAWN_FILE})"
   ALARM_TIME="$(echo ${ALARM_DATE} | awk '{print $3}')"
-  VOULME="${DAWN_VOLUME}"
+  VOULME=${DAWN_VOLUME}
 elif [[ $1 == "sunset" ]]; then
   date  >> ${LOG_FILE}
   ALARM_DATE="$(grep "${CURR_MONTH_DAY}" ${SUNSET_FILE})"
   ALARM_TIME="$(echo ${ALARM_DATE} | awk '{print $3}')"
-  VOULME="${SUNSET_VOLUME}"
+  VOULME=${SUNSET_VOLUME}
 else
   date  >> ${LOG_FILE}
   echo "the Ramadan script is failed!" >> ${LOG_FILE}
@@ -28,7 +28,7 @@ fi
 while true; do
   if [[ ${ALARM_TIME} == $(date | awk '{print $4}') ]]; then
     date  >> ${LOG_FILE}
-    bash -c "amixer -D pulse sset Master ${VOULME}\%; DISPLAY=:0 cvlc ${ATHAN_FILE}; exit" 
+    bash -c "amixer -D pulse sset Master ${VOULME}\%; DISPLAY=:0 vlc ${ATHAN_FILE}; exit" 
   fi
 done
 
